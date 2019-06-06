96d22e83823ee8513e6aa32b758833d91744565a
Jacob Mosteller <jacobmosteller722@gmail.com>
2019-06-05 18:13:52 -0600

    updates

--- Server/Index.js
+++ Server/Index.js
@@ -7,6 +7,12 @@ const massive = require('massive');
 
 const app = express();
 
+massive(process.env.CONNECTION_STRING)
+    .then((dbInstance)=>{
+        app.set('db', dbInstance)
+        console.log('Db connected')
+    })
+
 const port = 7070 || process.env.PORT
 
 app.listen(port,()=>{
--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+//Components
+
 import Auth from './Components/Auth/Auth';
 import Dashboard from './Components/Dashboard/Dashboard';
 import Form from './Components/Form/Form';
 import Nav from './Components/Nav/Nav';
 import Post from './Components/Post/Post';
-import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-      <Auth/>
-      <Dashboard/>
-      <Form/>
-      <Nav/>
-      <Post/>
+     
     </div>
   );
 }
